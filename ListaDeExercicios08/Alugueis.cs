7ff6c769fdd490f99bd37d2ea3fc5d8bc0a6bbb1
FRianOD <rianoliveiracontato@gmail.com>
2023-01-24 14:26:28 -0300

    Adicionar arquivos de projeto.

---
+++ ListaDeExercicios08/Alugueis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ListaDeExercicios08
+{
+    internal class Alugueis
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public Alugueis(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+    }
+}
---
+++ ListaDeExercicios08/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace ListaDeExercicios08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aluguéis
+            
+            Alugueis[] quartos = new Alugueis[10];
+
+            Console.Write("Quantos aluguéis vão ser registrados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\nNome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                quartos[quarto] = new Alugueis(nome, email);
+            }
+            Console.WriteLine("\nAluguéis:");
+            for (int i = 0; i < quartos.Length; i++ )
+            {
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine($"{i}: {quartos[i]}");
+                }
+            }
+        }
+    }
+}
